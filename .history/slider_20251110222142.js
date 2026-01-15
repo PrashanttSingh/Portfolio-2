e5a2fe02f05ca1f3246f16640b50198a1560fe02
Its Prashant <Prashantsingh3993@gmail.com>
2026-01-15 10:43:15 +0530

    First commit

---
+++ .history/slider_20251110222142.js
@@ -0,0 +1,13 @@
+<script>
+let index = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide() {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle("active", i === index);
+    });
+    index = (index + 1) % slides.length;
+}
+
+setInterval(showSlide, 3000);
+</script>
